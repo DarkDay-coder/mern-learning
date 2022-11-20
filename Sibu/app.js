04359a1ddbb0d0344c5d2f8eda19d13c7c66a841
DarkDay-coder <dhitalsibu3@gmail.com>
2022-11-21 00:25:12 +0545

    post new data to existing json file as new tour

--- Sibu/app.js
+++ Sibu/app.js
@@ -19,6 +19,9 @@ const tours = JSON.parse(
 );
 
 app.get('/api/v1/tours', (req, res) => {
+   console.log(
+      'currently there are ' + tours[tours.length - 1].id + ' tour list'
+   );
    res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -28,6 +31,25 @@ app.get('/api/v1/tours', (req, res) => {
    });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+   console.log(req.body);
+   const newID = tours[tours.length - 1].id + 1;
+   const newTour = Object.assign({ ID: newID }, req.body);
+   tours.push(newTour);
+
+   fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(tours),
+      (err) => {
+         res.status(201).json({
+            status: 'success',
+            data: {
+               newTour,
+            },
+         });
+      }
+   );
+});
 
 const port = 5000;
 app.listen(port, 'localhost', () => {
