63a7a8f543d63d8678e5d7a149ded01f0a6fdcc6
DarkDay-coder <dhitalsibu3@gmail.com>
2022-11-20 20:56:56 +0545

    alternative for callback i.e. promise

--- Asynchronous_JS/index.js
+++ Asynchronous_JS/index.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
 const superagent = require('superagent');
+
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+   console.log('promoise resolved');
    console.log(`Breed: ${data}`);
 
    superagent
       .get(`https://dog.ceo/api/breed/${data}/images/random`)
-      .end((err, res) => {
+      .then((res) => {
          console.log(res.body.message);
-
          fs.writeFile('dog-image.txt', res.body.message, (err) => {
             console.log('random dog image: ');
          });
+      })
+      .catch((err) => {
+         console.log(err.message);
+         console.log('promoise reject');
       });
 });
