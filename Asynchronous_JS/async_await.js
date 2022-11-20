4e97cd9395d02d3ee1c601294a9838704a2e11f4
DarkDay-coder <dhitalsibu3@gmail.com>
2022-11-20 21:29:28 +0545

    promise handling using async await

---
+++ Asynchronous_JS/async_await.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+   return new Promise((resolve, reject) => {
+      fs.readFile(file, (err, data) => {
+         if (err) reject('I could not find that file');
+         resolve(data);
+      });
+   });
+};
+
+const writeFilePro = (file, data) => {
+   return new Promise((resolve, reject) => {
+      fs.writeFile(file, data, (err) => {
+         if (err) reject('I could not find that file');
+         resolve('Successfully wrote');
+      });
+   });
+};
+
+const getDogPic = async () => {
+   try {
+      const data = await readFilePro(`${__dirname}/dog.txt`);
+      console.log(`Breed: ${data}`);
+      const res = await superagent.get(
+         `https://dog.ceo/api/breed/${data}/images/random`
+      );
+      console.log(res.body.message);
+      await writeFilePro('dog-image.txt', res.body.message);
+      console.log('Random dog image saved successfully');
+   } catch (err) {
+      console.log(err);
+   }
+};
+getDogPic();
