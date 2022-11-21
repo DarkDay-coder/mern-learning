acde774ef95b884476789f93876ee588f47df970
DarkDay-coder <dhitalsibu3@gmail.com>
2022-11-21 07:18:00 +0545

    simple code to patch method added

--- Sibu/app.js
+++ Sibu/app.js
@@ -77,6 +77,21 @@ app.post('/api/v1/tours', (req, res) => {
    );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+   if(req.params.id *1 > tours.length){
+      return res.status(404).json({
+         status: 'failed',
+         message: 'Invalid id provided',
+   });
+};
+   res.status(200).json({
+      status: 'success',
+      data:{
+         tour: '<updated tour here>',
+      }
+   })
+})
+
 const port = 5000;
 app.listen(port, 'localhost', () => {
    console.log('listening on port ' + port);
