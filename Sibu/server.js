31d143a0bd0e4a857847ffc0fb275419d731df51
DarkDay <dhitalsibu3@gmail.com>
2022-11-22 20:19:21 +0545

    database connected successfully

--- Sibu/server.js
+++ Sibu/server.js
@@ -1,9 +1,19 @@
 /* eslint-disable prettier/prettier */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
 console.log(app.get('env'));
+const DB =
+   'mongodb+srv://sibu:rdsibu@cluster0.muj4e4t.mongodb.net/Tour-project';
+dotenv.config({ path: './config.env' });
+mongoose.connect(DB, (err) => {
+   if (err) {
+      console.error('Error while mongoDB connection');
+   } else {
+      console.log('MongoDB connected successfully');
+   }
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, 'localhost', () => {
