6f618d75814b4c469706f51765fc89aeef85f418
DarkDay <dhitalsibu3@gmail.com>
2022-11-22 21:57:15 +0545

    deleted dummy local code and imported tourModel

--- Sibu/controllers/tour.controller.js
+++ Sibu/controllers/tour.controller.js
@@ -1,18 +1,7 @@
+const TourModel = require('./../models/tour.model');
 const fs = require('fs');
-const tours = JSON.parse(
-   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+
 class TourController {
-   checkId = (req, res, next, val) => {
-      console.log(`Tour id is ${val}`);
-      if (req.params.id * 1 > tours.length) {
-         return res.status(404).json({
-            status: 'failed',
-            message: 'Invalid ID',
-         });
-      }
-      next();
-   };
    checkBody = (req, res, next) => {
       if (!req.body.name || !req.body.price) {
          return res.status(400).json({
@@ -23,49 +12,30 @@ class TourController {
       next();
    };
 
-   getAllTours = (req, res) => {
-      console.log('currently there are ' + tours.length + ' tours available');
-      res.status(200).json({
-         status: 'success',
-         results: tours.length,
-         data: {
-            tours,
-         },
-      });
-   };
+   getAllTours = (req, res) => {};
 
    getTourById = (req, res) => {
       console.log(req.params);
       console.log(req.requestTime);
       const id = req.params.id * 1;
-      const tour = tours.find((el) => el.id === id);
+
       res.status(200).json({
-         status: 'success',
-         result: tour.length,
-         requestedAt: req.requestTime,
-         data: {
-            tour,
-         },
+         // status: 'success',
+         // result: tour.length,
+         // requestedAt: req.requestTime,
+         // data: {
+         //    tour,
+         // },
       });
    };
 
    createTour = (req, res) => {
-      const newID = tours.length;
-      const newTour = Object.assign({ ID: newID }, req.body);
-      tours.push(newTour);
-
-      fs.writeFile(
-         `${__dirname}/../dev-data/data/tours-simple.json`,
-         JSON.stringify(tours),
-         (err) => {
-            res.status(201).json({
-               status: 'success',
-               data: {
-                  newTour,
-               },
-            });
-         }
-      );
+      res.status(201).json({
+         status: 'success',
+         data: {
+            newTour,
+         },
+      });
    };
 
    updateTourById = (req, res) => {
