b9a50a42680c09d883ad81abfd8062c6c0972836
DarkDay <dhitalsibu3@gmail.com>
2022-11-22 21:48:22 +0545

    tour model created and code re-factored

--- Sibu/server.js
+++ Sibu/server.js
@@ -16,37 +16,6 @@ mongoose.connect(DB, (err) => {
    }
 });
 
-const tourSchema = new mongoose.Schema({
-   name: {
-      type: String,
-      required: [true, 'A tour must have a name'],
-      unique: true,
-   },
-   rating: {
-      type: Number,
-      default: 4.5,
-   },
-   price: {
-      type: Number,
-      required: [true, 'A tour must have a price'],
-   },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-const testTour = new Tour({
-   name: 'The forest hiker on upper area',
-   // rating: 4.7,
-   price: 497,
-});
-testTour
-   .save()
-   .then((doc) => {
-      console.log(doc);
-   })
-   .catch((err) => {
-      console.log('ERROR 😎 ', err);
-   });
-
 const port = process.env.PORT || 5000;
 app.listen(port, 'localhost', () => {
    console.log(`Server is running using port number: ${port}`);
