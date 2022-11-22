bc092a6b9f9478b562bed7859c650fabd7c6fa87
DarkDay <dhitalsibu3@gmail.com>
2022-11-22 20:48:48 +0545

    tour model is defined using mongoose schema

--- Sibu/server.js
+++ Sibu/server.js
@@ -16,6 +16,24 @@ mongoose.connect(DB, (err) => {
    }
 });
 
+const tourSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true
+   },
+   rating: {
+      type: Number,
+      default: 4.5,
+   },
+   price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+   },
+});
+
+const Tour = mongoose.model('Tour', tourSchema)
+
 const port = process.env.PORT || 5000;
 app.listen(port, 'localhost', () => {
    console.log(`Server is running using port number: ${port}`);
