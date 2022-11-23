a23f86024846705b7a64622d014b9e225316ce95
DarkDay <dhitalsibu3@gmail.com>
2022-11-23 12:39:43 +0545

    CRUD operation on tour model completed

--- Sibu/controllers/tour.controller.js
+++ Sibu/controllers/tour.controller.js
@@ -1,57 +1,98 @@
 const TourModel = require('./../models/tour.model');
 const fs = require('fs');
+const Tour = require('./../models/tour.model');
 
 class TourController {
-   checkBody = (req, res, next) => {
-      if (!req.body.name || !req.body.price) {
-         return res.status(400).json({
+   createTour = async (req, res) => {
+      // const newTour = new TourModel({});
+      // newTour.save();
+
+      try {
+         const newTour = await TourModel.create(req.body);
+         res.status(201).json({
+            status: 'success',
+            data: {
+               newTour,
+            },
+         });
+      } catch (error) {
+         res.status(400).json({
             status: 'failed',
-            message: 'Missing Name or Price',
+            message: 'Invalid data sent!!',
          });
       }
-      next();
    };
 
-   getAllTours = (req, res) => {};
-
-   getTourById = (req, res) => {
-      console.log(req.params);
-      console.log(req.requestTime);
-      const id = req.params.id * 1;
-
-      res.status(200).json({
-         // status: 'success',
-         // result: tour.length,
-         // requestedAt: req.requestTime,
-         // data: {
-         //    tour,
-         // },
-      });
+   getAllTours = async (req, res) => {
+      try {
+         const tours = await TourModel.find();
+         res.status(200).json({
+            status: 'succeed!',
+            result: tours.length,
+            data: tours,
+         });
+      } catch (error) {
+         res.status(404).json({
+            status: 'failed',
+            message: error,
+         });
+      }
    };
 
-   createTour = (req, res) => {
-      res.status(201).json({
-         status: 'success',
-         data: {
-            newTour,
-         },
-      });
+   getTourById = async (req, res) => {
+      try {
+         console.log('requested data is having id: ' + req.params.id);
+         const tour = await TourModel.findById(req.params.id); // TourModel.findOne({_id : req.params.id})
+         res.status(200).json({
+            status: 'succeed!',
+            data: tour,
+         });
+      } catch (error) {
+         res.status(404).json({
+            status: 'failed',
+            message: error + 'error',
+         });
+      }
    };
 
-   updateTourById = (req, res) => {
-      res.status(200).json({
-         status: 'success',
-         data: {
-            tour: '<updated tour here>',
-         },
-      });
+   updateTourById = async (req, res) => {
+      console.log('the update request is for id: ' + req.params.id);
+      try {
+         const tour = await TourModel.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+               upsert: true,
+               new: true,
+               runValidators: true,
+            }
+         );
+         res.status(200).json({
+            status: 'success',
+            data: tour,
+         });
+      } catch (error) {
+         res.status(404).json({
+            status: 'failed',
+            message: error + 'error',
+         });
+      }
    };
 
-   deleteTourById = (req, res) => {
-      res.status(204).json({
-         status: 'success',
-         data: null,
-      });
+   deleteTourById = async (req, res) => {
+      console.log('the delete request is for id: ' + req.params.id);
+      try {
+         const tour = await TourModel.findByIdAndDelete(req.params.id);
+         res.status(204).json({
+            status: 'success',
+            data: null,
+         });
+      } catch (error) {
+         res.status(404).json({
+            status: 'failed',
+            message: error + 'error',
+         });
+      }
    };
 }
 module.exports = TourController;
--- Sibu/routes/tour.routes.js
+++ Sibu/routes/tour.routes.js
@@ -4,17 +4,10 @@ const TourController = require('./../controllers/tour.controller');
 const tour_cont = new TourController();
 
 //param middleware
-router.param('id', tour_cont.checkId);
+// router.param('id', tour_cont.checkId);
 
-// create a check body middleware
-// check if the body contains the name and price properties
-// if not send 400(bad request) as response
-// add it to post handler stack
-
-router
-   .route('/')
-   .get(tour_cont.getAllTours)
-   .post(tour_cont.checkBody, tour_cont.createTour);
+// CRUD operation
+router.route('/').get(tour_cont.getAllTours).post(tour_cont.createTour);
 router
    .route('/:id')
    .get(tour_cont.getTourById)
