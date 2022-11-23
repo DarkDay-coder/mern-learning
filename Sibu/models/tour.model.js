7268111a0637105cbc68709d1e8a21710b473c8f
DarkDay <dhitalsibu3@gmail.com>
2022-11-23 13:24:21 +0545

    re-modeled the tour model to fit real time dataset

--- Sibu/models/tour.model.js
+++ Sibu/models/tour.model.js
@@ -4,14 +4,55 @@ const tourSchema = new mongoose.Schema({
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
+      trim: true,
    },
-   rating: {
+   description: {
+      type: String,
+      trim: true,
+   },
+   summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true, //removes all the whitespaces of beginning and ending
+   },
+   startDates: [Date],
+   duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+   },
+   maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+   },
+   difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+   },
+   ratingsAverage: {
       type: Number,
       default: 4.5,
+   },
+   ratingsQuantity: {
+      type: Number,
+      default: 0,
    },
    price: {
       type: Number,
       required: [true, 'A tour must have a price'],
+   },
+   priceDiscount: Number,
+   imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+   },
+   images: [String],
+   createdAt: {
+      type: Date,
+      default: Date.now(),
+   },
+   updatedAt: {
+      type: Date,
+      default: Date.now(),
    },
 });
 
