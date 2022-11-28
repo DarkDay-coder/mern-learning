19c4aa60de76707a593f189e2481661c34bbd4d4
DarkDay <dhitalsibu3@gmail.com>
2022-11-28 19:26:25 +0545

    helmet middleware for header cleaning

--- Sibu/app.js
+++ Sibu/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const apiError = require('./middleware/apiError.middleware');
 const globalErrorHandler = require('./controllers/apierror.controller');
 
@@ -10,6 +11,10 @@ const tourRouter = require('./routes/tour.routes');
 const userRouter = require('./routes/user.routes');
 
 // 1) GLOBAL MIDDLEWARE
+// Security http headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV !== 'production') {
    app.use(morgan('dev'));
    // app.use(morgan('tiny'));
@@ -18,25 +23,27 @@ if (process.env.NODE_ENV !== 'production') {
    // app.use(morgan('short'));
 }
 
+// Limit request from same IP
 const limiter = rateLimit({
    max: 10,
-   windowMs: 60 * 60 * 1000,
+   windowMs: 3600000,
    message: 'Too many request from this IP please try again in an hour',
 });
 app.use('/api', limiter);
 
 // DATA PARSER MIDDLEWARE
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 app.use(
    express.urlencoded({
       extended: false,
    })
 );
 
-// PUBLISHING PUBLIC ASSETS
+// PUBLISHING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
 
 // 2) CUSTOM MIDDLEWARE
+// test middleware
 app.use((req, res, next) => {
    console.log('1. Hello from my own middleware 👋');
    next();
