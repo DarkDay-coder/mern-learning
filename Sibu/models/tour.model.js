2575e5c63d0d20f2d4f4b2577edc9f08a9ffdaba
DarkDay <dhitalsibu3@gmail.com>
2022-11-30 12:07:12 +0545

    geo data scheme added for locations

--- Sibu/models/tour.model.js
+++ Sibu/models/tour.model.js
@@ -90,6 +90,30 @@ const tourSchema = new mongoose.Schema(
          default: Date.now(),
          select: false,
       },
+      startLocation: {
+         // GeoJSON
+         type: {
+            type: String,
+            default: 'Point',
+            enum: ['Point'],
+         },
+         coordinates: [Number],
+         address: String,
+         description: String,
+      },
+      locations: [
+         {
+            type: {
+               type: String,
+               default: 'Point',
+               enum: ['Point'],
+            },
+            coordinates: [Number],
+            address: String,
+            description: String,
+            day: Number,
+         },
+      ],
    },
    {
       toJSON: { virtuals: true },
