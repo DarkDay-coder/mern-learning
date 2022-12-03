15aa72597a4d021c3e8f0563976b63e382b2bac2
DarkDay <dhitalsibu3@gmail.com>
2022-12-03 23:56:46 +0545

    created and routed the home page with dynamic content

--- Tour-project-with-pug/controllers/view.controller.js
+++ Tour-project-with-pug/controllers/view.controller.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+//
+const TourModel = require('./../models/tour.model');
+const catchAsync = require('./../middleware/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+   // 1) Get tour data from collection
+   const tours = await TourModel.find();
+   // 2) Build template
+
+   // 3) render that template
    res.status(200).render('overview', {
       title: 'All Tours',
+      tours,
    });
-};
+});
 exports.getTour = (req, res) => {
    res.status(200).render('tour', {
       title: 'The Forest Hiker',
--- Tour-project-with-pug/routes/view.route.js
+++ Tour-project-with-pug/routes/view.route.js
@@ -1,11 +1,5 @@
 const router = require('express').Router();
 const viewController = require('./../controllers/view.controller');
-router.get('/', (req, res) => {
-   res.status(200).render('base', {
-      title: 'Home',
-      user: 'sibu',
-   });
-});
 router.get('/', viewController.getOverview);
 router.get('/tour', viewController.getTour);
 module.exports = router;
