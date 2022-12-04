efdd158ff33919c8af029a71acbf2f0364b513b2
DarkDay <dhitalsibu3@gmail.com>
2022-12-04 14:09:56 +0545

    reading cookies jwt token

--- Tour-project-with-pug/middleware/auth.middleware.js
+++ Tour-project-with-pug/middleware/auth.middleware.js
@@ -12,6 +12,8 @@ class authMiddleware {
          req.headers.authorization.startsWith('Bearer')
       ) {
          token = req.headers.authorization.split(' ')[1];
+      } else if (req.cookies.jwt) {
+         token = req.cookies.jwt;
       }
       if (!token) {
          return next(
